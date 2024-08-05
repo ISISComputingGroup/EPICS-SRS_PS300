3b1bdfcf468475789fe24e807ac9ba62beab3e0b
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-08-05 14:12:42 +0100

    run safe ruff fixes

--- system_tests/tests/ps300.py
+++ system_tests/tests/ps300.py
@@ -4,8 +4,7 @@ from parameterized import parameterized
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
-from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
-
+from utils.testing import get_running_lewis_and_ioc
 
 DEVICE_PREFIX = "PS300_01"
 
